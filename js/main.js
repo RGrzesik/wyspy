075a9c902e7dc1056e73af61243daf2bad0674e5
Rafal <rafcio105@onet.pl>
2021-05-14 12:03:44 +0200

    finished website

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ const nav = document.querySelector('.nav')
 const burgerBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
 
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSection = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
+
+
 function popup() {
     nav.classList.toggle('nav--active')
-
+    navBtnBars.classList.remove('black-bards-color');
     allNavItems.forEach(item => {
 
         item.addEventListener('click', () => {
@@ -24,4 +30,25 @@ const handleNavItemsAnimation = () => {
     })
 
 }
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+    allSection.forEach(section => {
+        if (section.classList.contains('white-section') &&
+            section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bards-color')
+        } else if (!section.classList.contains('white-section') &&
+            section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bards-color')
+        }
+    })
+
+}
+
+
+handleCurrentYear();
 burgerBtn.addEventListener('click', popup)
+window.addEventListener('scroll', handleObserver)
